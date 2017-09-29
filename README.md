d3b4958bb25c88df5bb316db2eaaf8822be7bbcc
BrandonRomano <BrandonRRomano@gmail.com>
2017-09-29 11:34:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<a href="https://engineering.carrot.is/"><p align="center"><img src="https://cloud.githubusercontent.com/assets/2105067/24525319/d3d26516-1567-11e7-9506-7611b3287d53.png" alt="Go Carrot" width="350px" align="center;" /></p></a>
+# FSM Emitable
+
+Emitable contains all of the types that are recommended for all [fsm](https://github.com/go-carrot/fsm) chatbot targets to implement in their emitter.
+
+# License
+
+[MIT](LICENSE.md)
---
+++ emitable.go
@@ -0,0 +1,41 @@
+/*
+Package emitable defines all of the types that are recommended
+for all fsm chatbot targets to implement in their emitter.
+
+All emitters should also handle strings, in addition to what
+is defined here.
+*/
+package emitable
+
+// Audio is a struct that represents an audio file to be emitted
+type Audio struct {
+	URL string
+}
+
+// File is a struct that represents a generic file to be emitted
+type File struct {
+	URL string
+}
+
+// Image is a struct that represents an image to be emitted
+type Image struct {
+	URL string
+}
+
+// Video is a struct that represents an video to be emitted
+type Video struct {
+	URL string
+}
+
+// QuickReply is a struct that represents an array of
+// possible responses from a user
+type QuickReply struct {
+	Message string
+	Replies []string
+}
+
+// Typing is a struct to represent that the bot is typing.
+// It is expected to be enabled and disabled
+type Typing struct {
+	Enabled bool
+}
